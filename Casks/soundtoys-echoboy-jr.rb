800680eccf7dc9268af471294979fd2ffc5926ef
Fotis Gimian <fgimiansoftware@gmail.com>
2017-04-09 21:52:56 +1000

    Updated Soundtoys software to reflect what is currently owned after the bundle sale

---
+++ Casks/soundtoys-echoboy-jr.rb
@@ -0,0 +1,23 @@
+require 'open-uri'
+
+base_dir = ENV.fetch('HOMEBREW_CASK_MUSIC_SOFTWARE_BASEDIR', '')
+
+cask 'soundtoys-echoboy-jr' do
+  version '5.2.0.12502'
+  sha256 'f73da1341b3babf5a744b6de5ab007628453f1e6280c740cfe2f4667cbad732f'
+
+  # :base_dir was verified as official when first introduced to the cask
+  url URI.encode("file://#{base_dir}/Soundtoys/EchoBoyJr5_#{version}.dmg")
+  name 'Soundtoys EchoBoy Jr.'
+  homepage 'https://www.soundtoys.com/product/echoboy-jr/'
+
+  pkg 'Install EchoBoy Jr..pkg'
+
+  uninstall pkgutil: 'com.soundtoys.com.soundtoys.EchoBoyJr5'
+
+  zap delete: [
+                '~/Library/Preferences/com.soundtoys.metrics.plist',
+                '~/Library/Preferences/com.soundtoys.notifications.plist',
+                '~/Library/Preferences/com.soundtoys.plist',
+              ]
+end
---
+++ Casks/soundtoys-sie-q.rb
@@ -0,0 +1,23 @@
+require 'open-uri'
+
+base_dir = ENV.fetch('HOMEBREW_CASK_MUSIC_SOFTWARE_BASEDIR', '')
+
+cask 'soundtoys-sie-q' do
+  version '5.2.0.12502'
+  sha256 '1c1b41dc75e2d4c0c31aa4c9c1ca3ccc7079bca7f56ccfd89a03f02897ff91fe'
+
+  # :base_dir was verified as official when first introduced to the cask
+  url URI.encode("file://#{base_dir}/Soundtoys/SieQ5_#{version}.dmg")
+  name 'Soundtoys Sie-Q 5'
+  homepage 'https://www.soundtoys.com/product/sie-q/'
+
+  pkg 'Install SieQ.pkg'
+
+  uninstall pkgutil: 'com.soundtoys.com.soundtoys.SieQ5'
+
+  zap delete: [
+                '~/Library/Preferences/com.soundtoys.metrics.plist',
+                '~/Library/Preferences/com.soundtoys.notifications.plist',
+                '~/Library/Preferences/com.soundtoys.plist',
+              ]
+end
