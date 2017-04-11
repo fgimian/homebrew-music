72e0ccc12f8a94a4bd3da1a5d39b040b666c44c2
Fotis Gimian <fgimiansoftware@gmail.com>
2017-04-11 14:56:46 +1000

    Updated Sausage Fattener to the correct SHA256 sum and package name

--- Casks/dada-life-sausage-fattener.rb
+++ Casks/dada-life-sausage-fattener.rb
@@ -4,14 +4,14 @@ base_dir = ENV.fetch('HOMEBREW_CASK_MUSIC_SOFTWARE_BASEDIR', '')
 
 cask 'dada-life-sausage-fattener' do
   version '1.1.5'
-  sha256 '9ade0e2bf828604cdb0bf1d119ad2e03bfdc20adb6d2c4afe0fe6814fff9c821'
+  sha256 '5837ebefdcece276bcb38ee8eb5f3247e393c9380e94d2a8a0fa2400638cec81'
 
   # :base_dir was verified as official when first introduced to the cask
   url URI.encode("file://#{base_dir}/Dada Life/SausageFattener-#{version}-Installer-Fotis-Gimian.dmg")
   name 'Dada Life Sausage Fattener'
   homepage 'http://www.tailorednoise.com/index.php?/plugins/sausage-fattener-by-dada-life/'
 
-  pkg 'Sausage-Fattener-Installer.pkg'
+  pkg 'SausageFattener-Installer.pkg'
 
   uninstall pkgutil: 'com.tailorednoise.SausageFattenerInstaller.*'
 
