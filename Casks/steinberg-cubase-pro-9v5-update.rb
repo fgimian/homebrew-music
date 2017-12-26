4ed41381f648ed89627b15aa601c2ec7ba614b06
Fotis Gimian <fgimiansoftware@gmail.com>
2017-12-26 13:05:43 +1100

    Updated Steinberg Cubase related casks to the new to 9.5 release

--- Casks/steinberg-cubase-pro-9v5-update.rb
+++ Casks/steinberg-cubase-pro-9v5-update.rb
@@ -2,16 +2,16 @@ require 'open-uri'
 
 base_dir = ENV.fetch('HOMEBREW_CASK_MUSIC_SOFTWARE_BASEDIR', '')
 
-cask 'steinberg-cubase-pro-9-update' do
-  version '9.0.10'
-  sha256 '773896bf97a8135ecfa38c815fa83caa4e54443ef84aa4d085a1c89af13e02cb'
+cask 'steinberg-cubase-pro-9v5-update' do
+  version '9.5.10'
+  sha256 '122ff408ca67725b507c9780bd8c3208461b036e9300972f46b55a7b79e59595'
 
   # :base_dir was verified as official when first introduced to the cask
   url URI.encode("file://#{base_dir}/Steinberg/Cubase_#{version}_Update_mac.dmg")
-  name 'Steinberg Cubase Pro (Update)'
+  name 'Steinberg Cubase Pro 9.5 (Update)'
   homepage 'https://www.steinberg.net/en/products/cubase'
 
-  depends_on cask: 'steinberg-cubase-pro-9'
+  depends_on cask: 'steinberg-cubase-pro-9v5'
 
-  pkg 'Cubase 9 Update.pkg'
+  pkg 'Cubase 9.5 Update.pkg'
 end
---
+++ Casks/steinberg-cubase-pro-9v5.rb
@@ -0,0 +1,63 @@
+require 'open-uri'
+
+base_dir = ENV.fetch('HOMEBREW_CASK_MUSIC_SOFTWARE_BASEDIR', '')
+
+cask 'steinberg-cubase-pro-9v5' do
+  version '9.5.0'
+  sha256 'b7a6fe2cb0ba266bb0827977298046ec8f1bf64017299c310ddaa7bf96496e4c'
+
+  # :base_dir was verified as official when first introduced to the cask
+  url URI.encode("file://#{base_dir}/Steinberg/Cubase_9.5_Installer_mac.dmg")
+  name 'Steinberg Cubase Pro 9.5'
+  homepage 'https://www.steinberg.net/en/products/cubase'
+
+  pkg 'Cubase for Mac/Cubase 9.5.pkg'
+
+  uninstall pkgutil: [
+                       'com.steinberg.*.cubase95',
+                       'com.steinberg.AcousticAgentSEMixer',
+                       'com.steinberg.AcousticAgentSEPatternBanks',
+                       'com.steinberg.AcousticAgentSEStyleFiles',
+                       'com.steinberg.Cubase9v5',
+                       'com.steinberg.Documentation.hsse3',
+                       'com.steinberg.Documentation.retrologue',
+                       'com.steinberg.GrooveAgentFamily-SubPresetsShared',
+                       'com.steinberg.GrooveAgentSE-SubPresets',
+                       'com.steinberg.GrooveAgentSE',
+                       'com.steinberg.HALionFamily-SubPresetsShared',
+                       'com.steinberg.HALionIcon',
+                       'com.steinberg.HALionLibraryManager',
+                       'com.steinberg.HALionSonicSE-SubPresets',
+                       'com.steinberg.HALionSonicSE.hsse3',
+                       'com.steinberg.Padshop',
+                       'com.steinberg.PadshopProVST3Presets',
+                       'com.steinberg.PadshopSubPresets',
+                       'com.steinberg.PadshopVST3Presets',
+                       'com.steinberg.ReWire',
+                       'com.steinberg.Retrologue',
+                       'com.steinberg.Retrologue2VST3Presets',
+                       'com.steinberg.RetrologueSubPresets',
+                       'com.steinberg.RetrologueVST3Presets',
+                       'com.steinberg.RockPopToolboxMIDIFiles',
+                       'com.steinberg.RockPopToolboxPatternBanks',
+                       'com.steinberg.UploadManager',
+                       'com.steinberg.euconappsets',
+                       'com.steinberg.euconvstxmls',
+                       'com.steinberg.nuagevstxmls',
+                       'com.steinberg.padshop.documentation',
+                       'com.steinberg.vst3vsttransitextension',
+                       'com.steinberg.vsttransitcore',
+                       # The following relate to Retrologue & Padshop installers
+                       'com.steinberg.Documentation',
+                       'com.steinberg.PadshopAU',
+                       'com.steinberg.PadshopAU.padshop',
+                       'com.steinberg.RetrologueAU.retrologue',
+                     ]
+
+  zap delete: [
+                '~/Library/Caches/com.steinberg.cubase9v5',
+                '~/Library/Preferences/Application Files/Cubase 9.5',
+                '~/Library/Preferences/com.steinberg.cubase9v5.plist',
+                '~/Library/Preferences/Cubase 9.5',
+              ]
+end
