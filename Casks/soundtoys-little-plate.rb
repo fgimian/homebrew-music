d63f4011e52f99bb0173464fb1d4e962a7668e16
Fotis Gimian <fgimiansoftware@gmail.com>
2018-03-03 13:08:29 +1100

    Corrected a little typo in the version of Soundtoys Little Plate

--- Casks/soundtoys-little-plate.rb
+++ Casks/soundtoys-little-plate.rb
@@ -3,11 +3,11 @@ require 'open-uri'
 base_dir = ENV.fetch('HOMEBREW_CASK_MUSIC_SOFTWARE_BASEDIR', '')
 
 cask 'soundtoys-little-plate' do
-  version '5_5.2.2.13206'
+  version '5.2.2.13206'
   sha256 'b3eb5c56777d8ebcd45c162afa94c28d3171294f16602b49ca5acc5caa2d1767'
 
   # :base_dir was verified as official when first introduced to the cask
-  url URI.encode("file://#{base_dir}/Soundtoys/LittlePlate#{version}.dmg")
+  url URI.encode("file://#{base_dir}/Soundtoys/LittlePlate5_#{version}.dmg")
   name 'Soundtoys Little Plate'
   homepage 'http://www.soundtoys.com/product/little-plate/'
 
