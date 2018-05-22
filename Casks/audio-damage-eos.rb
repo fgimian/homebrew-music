a3fa6414381927d4e3b2d10544c61c897d229dfd
Fotis Gimian <fgimiansoftware@gmail.com>
2018-05-22 23:07:15 +1000

    Updated Audio Damage Eos cask to the new major version

--- Casks/audio-damage-eos.rb
+++ Casks/audio-damage-eos.rb
@@ -3,15 +3,15 @@ require 'open-uri'
 base_dir = ENV.fetch('HOMEBREW_CASK_MUSIC_SOFTWARE_BASEDIR', '')
 
 cask 'audio-damage-eos' do
-  version '1.1.2'
-  sha256 'dc92482376f45de217f279dbc4a43e07ad952910c97a2ddd5e83a8ffe75097cb'
+  version '2.0.4.1'
+  sha256 'f187856d559c5eb37cd6001d884fb4e4f353a3f27c858fc8405ba5cd33f3de08'
 
   # :base_dir was verified as official when first introduced to the cask
-  url URI.encode("file://#{base_dir}/Audio Damage/OSX_eos_#{version.no_dots}.zip")
+  url URI.encode("file://#{base_dir}/Audio Damage/AD034_Eos_#{version.no_dots}.zip")
   name 'Audio Damage Eos'
-  homepage 'https://www.audiodamage.com/effects/product.php?pid=AD023'
+  homepage 'https://www.audiodamage.com/products/ad034-eos-2'
 
-  pkg "Eos_#{version.no_dots}.pkg"
+  pkg "AD034_Eos_#{version.no_dots}/OSX_Eos #{version.no_dots}.pkg"
 
-  uninstall pkgutil: 'com.audiodamage.pkg.Eos-*'
+  uninstall pkgutil: 'com.audiodamage.pkg.*'
 end
